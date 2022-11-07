9508463d97fd9a828351b06d3f8278f8646fda51
DR_INFINITY_92 <d.rajkovicrs.o@gmail.com>
2022-11-07 13:37:57 +0100

    07.11.2022.

---
+++ Grananje/proizvod cifara unetih kao znak.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char x;
+    int broj,proizvod=1,br=3,i=3;
+    p:
+    printf("unesi celi broj\n");
+    scanf(" %c",&x);
+    if(x>=48 && x<=57)
+    {
+        broj=x-48;
+        proizvod=proizvod*broj;
+        i--;
+    }
+    else 
+    {
+        br--;
+        i--;
+    }
+    if(i!=0)
+        goto p;
+    if(br==0)
+        printf("medju unetim znakovima nema cifara");
+    else 
+        printf("proizvod=%d",proizvod);
+
+return 0;
+}

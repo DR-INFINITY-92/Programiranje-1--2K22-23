9e2f0b3ca0cbcaa6d791b5060cfe5d5790b99231
DR_INFINITY_92 <d.rajkovicrs.o@gmail.com>
2022-11-08 12:03:18 +0100

    08.11.

---
+++ Grananje/Z-1.2.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    float x;
+    p:
+    printf("unesi realni broj\n");
+    scanf("%f", &x);
+    if(x==0)
+    {
+        printf("nije dozvoljeno deljenje nulom\n");
+        goto p;
+    }
+    else
+        printf("reciprocna vrednost broja x je = %.4f",1/x);
+
+}

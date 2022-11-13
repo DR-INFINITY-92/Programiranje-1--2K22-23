301cf832aba96489618e79c07a893280ba100578
DR_INFINITYgit config --global user.email d.rajkovicrs@gmail.com <d.rajkovicrs@gmail.comL>
2022-11-13 09:13:00 +0100

    c

--- Grananje/tempCodeRunnerFile.c
+++ Grananje/tempCodeRunnerFile.c
@@ -2,28 +2,12 @@
 #include<stdlib.h>
 int main()
 {
-    char x;
-    printf("unesi malo ili veliko slovo abecede\n");
-    scanf(" %c",&x);
-    if(x>='a'&&x<='z')
-    {
-        printf("uneti znak je %c\n",x);
-        printf("njegov ascii kod je %d\n",x);
-        printf("Odgovarajuce veliko slovo je %c\nnjegov ascii kod je %d\n",x-32,x-32);
-    }
+    float x;
+    printf("unesi realni broj\n");
+    scanf("%f", &x);
+    if(x<0)
+        printf("apsolutna vrednost broja x je = %.2f",(-1)*x);
     else
-    {
-        if(x>='A'&&x<='Z')
-        {
-            printf("uneti znak je %c\n",x);
-            printf("njegov ascii kod je %d\n",x);
-            printf("Odgovarajuce malo slovo je %c\nnjegov ascii kod je %d\n",x+32,x+32);
-        }
-        else
-        {   
-            printf("uneti znak je %c\n",x);
-            printf("njegov ascii kod je %d\n",x);
-        }
-    }
+        printf("apsolutna vrednost broja x je = %.2f",x);
     return 0;
 }

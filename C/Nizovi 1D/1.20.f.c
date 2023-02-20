76b5cee55fa5d2b9b50055e4033134a77fb83a55
DR_INFINITY <d.rajkovicrs@gmail.com>
2023-02-20 13:35:49 +0100

    x

---
+++ C/Nizovi 1D/1.20.f.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+void unos_niza(int n, int a[n])
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("unesi %d. element niza\n",i+1);
+        scanf("%d",&a[i]);
+    }
+}
+
+void ispis_niza(int n, int a[n])
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+void proizvod_clanova_niza_neparni_indeks(int n, int a[n])
+{
+    int i,p=1;
+    for(i=0;i<n;i++)
+        if(i%2!=0)
+            p=p*a[i];
+        
+    printf("proizvod elemnata niza je %d",p);
+}
+void broj_elemenata_niza(int *n)
+{ 
+    printf("unesi koliko elemenata ima niz\n");
+    scanf("%d",n);
+}
+void main()
+{
+    int n,i;
+    broj_elemenata_niza(&n);
+    int a[n];
+    unos_niza(n,a);
+    printf("elemenit polaznog niza su\n");
+    ispis_niza(n,a);
+    proizvod_clanova_niza_neparni_indeks(n,a);
+}

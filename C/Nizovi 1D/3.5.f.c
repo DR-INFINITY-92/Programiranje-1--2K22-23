dd0c41d95561f0b26a0f1ed7e3807a855bf9eee8
DR_INFINITY <d.rajkovicrs@gmail.com>
2023-03-03 11:23:31 +0100

    X

---
+++ C/Nizovi 1D/3.5.f.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void unos_niza(int n, int a[n])
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("unesi %d. element niza\n",i+1);
+        scanf("%d",&a[i]);
+    }
+}
+
+void ispis_niza(int n, int a[n])
+{
+    int i;
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+    printf("\n");
+}
+
+void broj_elemenata_niza(int *n)
+{ 
+    printf("unesi koliko elemenata ima niz\n");
+    scanf("%d",n);
+}
+void kreiranje_niza_b(int n, int a[n],int b[n])
+{
+    int i,x;
+    for(i=0;i<n/2;i++)
+        b[i]=a[i]+a[n-1-i];
+
+}
+
+void main()
+{
+    int n;
+    broj_elemenata_niza(&n);
+    int a[n],b[n/2];
+    unos_niza(n,a);
+    printf("elementi niza a:\n");
+    ispis_niza(n,a);
+    kreiranje_niza_b(n,a,b);
+    printf("elementi niza b:\n");
+    ispis_niza(n/2,b);
+}
+
+
